8a0521231d6ca6ce19264c26372534c79b9d989a
Tobias <tobias.aditya@gmail.com>
2022-09-02 16:01:56 +0700

    use new deployed factory contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,6 +1,8 @@
 import web3 from './web3';
 import CampaignFactory from "./build/CampaignFactory.json";
-
-const instance = new web3.eth.Contract(JSON.parse(CampaignFactory.interface), '0xC4Ce3B068035591905727396AdF3E25bAbec5909');
+//Old Contract
+// const instance = new web3.eth.Contract(JSON.parse(CampaignFactory.interface), '0xC4Ce3B068035591905727396AdF3E25bAbec5909');
+//New contract (with function summary and get approval count)
+const instance = new web3.eth.Contract(JSON.parse(CampaignFactory.interface), '0x95C6D23afE31cf0F31542749D03656410E665b29');
 
 export default instance;
