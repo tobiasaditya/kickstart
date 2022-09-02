6f2cd94c8004e03b18f9361ad713984e494e3a93
Tobias <tobias.aditya@gmail.com>
2022-09-02 14:32:57 +0700

    add create campaign page

---
+++ pages/campaigns/new.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { Button, Form, Input, Message } from "semantic-ui-react";
+import Layout from "../../components/Layout"
+import factory from "../../ethereum/factory"
+import web3 from "../../ethereum/web3"
+class CampaignNew extends Component {
+
+    state = {
+        minimumContribution: "",
+        errorMessage: "",
+        loading: false
+    }
+
+    onSubmit = async (event) => {
+        event.preventDefault()
+        this.setState({ loading: true, errorMessage: "" })
+        try {
+            const accounts = await web3.eth.getAccounts()
+            await factory.methods.createCampaign(this.state.minimumContribution).send({ from: accounts[0] })
+        } catch (err) {
+            this.setState({ errorMessage: err.message })
+        }
+        this.setState({ loading: false })
+
+    }
+
+    render() {
+        return (
+            <Layout>
+                <h1>This is a new campaign</h1>
+                <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+                    <Form.Field>
+                        <label>Minimum Contribution</label>
+                        <Input
+                            placeholder='0'
+                            labelPosition='right'
+                            label="wei"
+                            value={this.state.minimumContribution}
+                            onChange={(event) => { this.setState({ minimumContribution: event.target.value }) }}
+                        />
+                    </Form.Field>
+                    <Message error header="Oops!" content={this.state.errorMessage}></Message>
+                    <Button primary type='submit' loading={this.state.loading}>Submit</Button>
+                </Form>
+            </Layout>
+        )
+    }
+}
+
+export default CampaignNew
