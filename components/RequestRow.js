934b424f75e61af1fe3414b55c290a5bd63d3022
Tobias <tobias.aditya@gmail.com>
2022-09-03 22:49:02 +0700

    implement approveRequest method

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
-import { Table } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
 import web3 from "../ethereum/web3";
-
+import Campaign from "../ethereum/campaign"
 class RequestRow extends Component {
+
+    onApprove = async (event) => {
+        event.preventDefault()
+        const campaign = Campaign(this.props.address)
+        const accounts = await web3.eth.getAccounts()
+        await campaign.methods.approveRequest(this.props.id).send({ from: accounts[0] })
+    }
+
     render() {
         const { Row, Cell } = Table
         const { id, request, approversCount } = this.props
@@ -13,8 +21,16 @@ class RequestRow extends Component {
                 <Cell>{web3.utils.fromWei(request.amount, 'ether')}</Cell>
                 <Cell>{request.recipient}</Cell>
                 <Cell>{request.approvalCount}/{approversCount}</Cell>
-                <Cell>Approve</Cell>
-                <Cell>Finalize</Cell>
+                <Cell>
+                    <Button color="green" basic onClick={this.onApprove}>
+                        Approve
+                    </Button>
+                </Cell>
+                <Cell>
+                    <Button secondary>
+                        Finalize
+                    </Button>
+                </Cell>
 
             </Row>
         )
