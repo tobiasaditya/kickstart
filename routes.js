31b5f638a623e5e8fcc9941a6944b0807696e549
Tobias <tobias.aditya@gmail.com>
2022-09-03 21:02:17 +0700

    add route show list request

--- routes.js
+++ routes.js
@@ -2,4 +2,5 @@ const routes = require('next-routes')();
 routes
     .add("/campaigns/new", "/campaigns/new")
     .add("/campaigns/:address", "/campaigns/show")
+    .add("/campaigns/:address/request", "/campaigns/request/index")
 module.exports = routes;
