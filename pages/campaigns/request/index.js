1440506946ea90f6812e662ef94d8fd6a069ce8a
Tobias <tobias.aditya@gmail.com>
2022-09-03 22:11:39 +0700

    get all request at once at init props

--- pages/campaigns/request/index.js
+++ pages/campaigns/request/index.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import { Button } from "semantic-ui-react";
 import Layout from "../../../components/Layout";
+import Campaign from "../../../ethereum/campaign";
 import { Link } from "../../../routes";
 class RequestIndex extends Component {
     static async getInitialProps(props) {
+        const campaign = Campaign(props.query.address)
+        const requestCount = await campaign.methods.getRequestCount().call()
+        console.log(requestCount)
+        //Call all request using 1 request!
+        const requests = await Promise.all(
+            Array(parseInt(requestCount)).fill().map((element, index) => {
+                return campaign.methods.requests(index).call()
+            })
+        )
+
+        console.log(requests)
+
         return {
-            address: props.query.address
+            address: props.query.address,
+            requests: requests,
+            requestCount: requestCount
         }
     }
     render() {
